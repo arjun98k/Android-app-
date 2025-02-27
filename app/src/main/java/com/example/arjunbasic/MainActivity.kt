8e1ca733019e2009efc64a48e432abda21c1e06c
arjun98k <arjunkandekar14@gmail.com>
2025-02-27 13:14:19 +0530

    first app

--- app/src/main/java/com/example/arjunbasic/MainActivity.kt
+++ app/src/main/java/com/example/arjunbasic/MainActivity.kt
@@ -5,6 +5,7 @@ package com.example.arjunbasic
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,14 +15,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnup = findViewById<Button>(R.id.btn)
-        btnup.setOnClickListener{
-            Toast.makeText(applicationContext, "Arjun kandekar",Toast.LENGTH_SHORT).show()
-            val btnup2 = findViewById<Button>(R.id.btn3)
+//        val btnup = findViewById<Button>(R.id.btn)
+//        btnup.setOnClickListener{
+//            Toast.makeText(applicationContext, "Arjun kandekar",Toast.LENGTH_SHORT).show()
+//            val btnup2 = findViewById<Button>(R.id.btn3)
+//
+//            btnup2.setOnClickListener{
+//                Toast.makeText(applicationContext, "My first app",Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
-            btnup2.setOnClickListener{
-                Toast.makeText(applicationContext, "My first app",Toast.LENGTH_SHORT).show()
+        val darkbtn =findViewById<Button>(R.id.btndark)
+        val layout = findViewById<LinearLayout>(R.id.linearlayout)
+
+        var isDark = false
+
+        darkbtn.setOnClickListener {
+            if (isDark){
+                layout.setBackgroundColor(R.color.white)
+            }else{
+                layout.setBackgroundColor(R.color.black)
             }
+            isDark = !isDark
         }
 
     }
