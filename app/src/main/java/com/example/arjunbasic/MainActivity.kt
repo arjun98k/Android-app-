85ab4e7d6424e27a823aac647b885e79d12bea89
arjun98k <arjunkandekar14@gmail.com>
2025-02-27 12:32:47 +0530

    first app

---
+++ app/src/main/java/com/example/arjunbasic/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.arjunbasic
+
+
+
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnup = findViewById<Button>(R.id.btn)
+        btnup.setOnClickListener{
+            Toast.makeText(applicationContext, "Arjun kandekar",Toast.LENGTH_SHORT).show()
+            val btnup2 = findViewById<Button>(R.id.btn3)
+
+            btnup2.setOnClickListener{
+                Toast.makeText(applicationContext, "My first app",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
